1bdea0ce6304e586f1d64b537bf82705833aceda
NguyênNguyên <billbatri085@gmail.com>
2024-11-18 20:58:14 +0700

    RM-03 | config fontFamily

--- src/App.jsx
+++ src/App.jsx
@@ -6,23 +6,33 @@ import Login from './pages/Login.jsx';
 import AdminLayout from './layouts/AdminLayout.jsx';
 import StaffLayout from './layouts/StaffLayout.jsx';
 import ManagerLayout from './layouts/ManagerLayout.jsx';
+import { ConfigProvider } from 'antd';
 const App = () => {
   // const [count, setCount] = useState(0)
 
   return (
-    <Routes>
-      <Route path={PATHS.HOME.HOMEPAGE} element={<Home />} />
-      <Route path={PATHS.HOME.LOGIN} element={<Login />} />
-      <Route path={PATHS.ADMIN.DASHBOARD} element={<AdminLayout />} >
+    <ConfigProvider theme={
+      {
+        token: {
+          fontFamily: "'Montserrat', sans-serif",
+        }
+      }
+    }>
+      <Routes>
+        <Route path={PATHS.HOME.HOMEPAGE} element={<Home />} />
+        <Route path={PATHS.HOME.LOGIN} element={<Login />} />
+        <Route path={PATHS.ADMIN.DASHBOARD} element={<AdminLayout />} >
 
-      </Route>
-      <Route path={PATHS.STAFF.DASHBOARD} element={<StaffLayout />} >
+        </Route>
+        <Route path={PATHS.STAFF.DASHBOARD} element={<StaffLayout />} >
 
-      </Route>
-      <Route path={PATHS.MANAGER.DASHBOARD} element={<ManagerLayout />} >
 
-      </Route>
-    </Routes>
+        </Route>
+        <Route path={PATHS.MANAGER.DASHBOARD} element={<ManagerLayout />} >
+
+        </Route>
+      </Routes>
+    </ConfigProvider>
   )
 }
 
