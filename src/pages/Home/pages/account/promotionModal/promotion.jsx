3fc5ce5fdbfabdffeca1362fa98e8b57a528467f
NguyênNguyên <billbatri085@gmail.com>
2024-11-24 18:57:07 +0700

    RM-09 | Fix UI and add voucher

--- src/pages/Home/pages/account/promotionModal/promotion.jsx
+++ src/pages/Home/pages/account/promotionModal/promotion.jsx
@@ -1,11 +1,11 @@
-import { Modal, Pagination } from "antd"
+import { Divider, Flex, Modal, Pagination } from "antd"
 import PropTypes from 'prop-types';
 import './Promotion.scss'
 import { useEffect, useState } from "react";
 import { getValidPromotion } from "../../../../../services/userService";
 import { formatCurrency, formatDate2 } from "../../../../../utils/format";
 
-const Promotion = ({ isOpen, onClose }) => {
+const Promotion = ({ isOpen, onClose, setFieldValue = null }) => {
 
     const [loading, setLoading] = useState(false);
     const [search, setSearch] = useState('');
@@ -17,30 +17,39 @@ const Promotion = ({ isOpen, onClose }) => {
     const onSearchChange = (e) => {
         setSearch(e.target.value);
     };
-    
+
     const handlePageChange = (page, pageSize) => {
         setCurrentPage(page);
         setPageSize(pageSize);
     };
 
     useEffect(() => {
+        console.log("Promotion modal is open: ", promotions);
+    }, [promotions]);
+    useEffect(() => {
         if (isOpen) {
-          fetchPromotions();
+            fetchPromotions();
         }
-      }, [currentPage, pageSize, search, isOpen]);
+    }, [currentPage, pageSize, search, isOpen]);
 
     const fetchPromotions = async () => {
         try {
-          setLoading(true);
-          const { data, total } = await getValidPromotion(currentPage, pageSize, search);
-          setPromotions(data);
-          setTotal(total);
-          setLoading(false);
+            setLoading(true);
+            const { data, total } = await getValidPromotion(currentPage, pageSize, search);
+            setPromotions(data);
+            setTotal(total);
+            setLoading(false);
         } catch (error) {
-          console.error('Error fetching foods:', error);
-          setLoading(false);
+            console.error('Error fetching foods:', error);
+            setLoading(false);
         }
-      };
+    };
+    const handleSelect = (promotion) => {
+        if (setFieldValue) {
+            setFieldValue(promotion); // Truyền trực tiếp giá trị hoặc đối tượng
+            onClose(); // Đóng modal
+        }
+    };
 
     return (
         <>
@@ -63,12 +72,12 @@ const Promotion = ({ isOpen, onClose }) => {
                 <div className="promotion-content">
                     <div className="promotion-action row">
                         <div className="col-6">
-                            <input type="text" 
-                                placeholder="Tìm mã khuyến mãi..." 
+                            <input type="text"
+                                placeholder="Tìm mã khuyến mãi..."
                                 onChange={(e) => {
-                                e.target.value = e.target.value.toUpperCase(); // Chuyển thành chữ hoa
-                                onSearchChange(e); // Gọi hàm xử lý tìm kiếm
-                            }}/>
+                                    e.target.value = e.target.value.toUpperCase(); // Chuyển thành chữ hoa
+                                    onSearchChange(e); // Gọi hàm xử lý tìm kiếm
+                                }} />
                         </div>
                     </div>
                     {loading ? (
@@ -78,24 +87,24 @@ const Promotion = ({ isOpen, onClose }) => {
                     ) : (
                         promotions.map((promotion) => (
                             <>
-                                <div className="list-promotion mt-3">
-                                    <div className="promotion-item row">
+                                <div className="list-promotion mt-3" >
+                                    <div className="promotion-item row" onClick={() => handleSelect(promotion)}>
                                         <div className="col-3 with-dashed-border d-flex flex-column justify-content-center">
                                             <p className={`text-center discount-fix ${promotion.type === "percentage" ? "discount-percentage" : "discount-fixed"}`}>
                                                 {promotion.type === "percentage" ? "Giảm tỉ lệ" : "Giảm trực tiếp"}
                                             </p>
-                                            <p 
+                                            <p
                                                 className={`text-center discount-value ${promotion.type === "percentage" ? "discount-percentage" : ""}`}
                                             >
-                                                {promotion.type === "fixed" 
-                                                ? `${formatCurrency(promotion.discount)}` 
-                                                : `${promotion.discount}%`
+                                                {promotion.type === "fixed"
+                                                    ? `${formatCurrency(promotion.discount)}`
+                                                    : `${promotion.discount}%`
                                                 }
                                             </p>
                                         </div>
                                         <div className="col-9 d-flex flex-column justify-content-center">
                                             <p className="discount-code"><strong className="me-2">Mã giảm giá:</strong> {promotion.code}</p>
-                                            <p className="discount-text"><strong className="me-2">Số lượng: </strong>{promotion.quantity}</p>
+                                            <Flex align="center"><p className="discount-text"><strong className="me-2">Số lượng: </strong>{promotion.quantity}</p> <Divider type="vertical" /> <p className="discount-text"><strong className="me-2">Điều kiện: </strong>{promotion.condition}</p></Flex>
                                             <p className="discount-text"><strong className="me-2">Ngày bắt đầu: </strong>{formatDate2(promotion.startDate)} - <strong> Ngày kết thúc:</strong> {formatDate2(promotion.endDate)}</p>
                                             <p className="discount-text"><strong className="me-2">Mô tả:</strong> {promotion.description} </p>
                                             <p className="discount-text"><strong className="me-2">Điều kiện:</strong>Hóa đơn giá trị trên {formatCurrency(promotion.condition)} </p>
--- src/pages/Staff/BookingDetails.jsx
+++ src/pages/Staff/BookingDetails.jsx
@@ -5,6 +5,7 @@ import { Button, Card, Col, Descriptions, Flex, Form, Input, List, message, Row,
 import styles from "./BookingDetails.module.scss";
 import { addOrderDetail, getListOrder, payment } from "../../services/apiService";
 import OrderModal from "../Home/pages/reservatePage/orderModal/orderModal";
+import Promotion from "../Home/pages/account/promotionModal/promotion";
 // import InfiniteScroll from "react-infinite-scroller";
 const { Title, Text } = Typography;
 
@@ -12,12 +13,16 @@ const BookingDetails = () => {
   const location = useLocation();
   const { item } = location.state || {};
   const [listFood, setListFood] = useState([]);
+  const [form] = Form.useForm();
+  const [promotion, setPromotion] = useState(null);
   const [total, setTotal] = useState(0);
   const [totalPrice, setTotalPrice] = useState(0);
   const [isOrderModalOpen, setIsOrderModalOpen] = useState(false);
   const [selectedFoods, setSelectedFoods] = useState([]);
   const [flag, setFlag] = useState(false);
-
+  const [tempPrice, setTempPrice] = useState(0);
+  const [isPromotionModalOpen, setIsPromotionModalOpen] = useState(false);
+  const closePromotionModal = () => setIsPromotionModalOpen(false);
   useEffect(() => {
     console.log("flag", flag);
     const order_detail = getSelectedFoodsMinimal();
@@ -90,10 +95,24 @@ const BookingDetails = () => {
   }, []);
 
   useEffect(() => {
+    if (promotion) {
+      // setTotal(total - promotion.discount);
+      form.setFieldsValue({ voucher: promotion.code });
+    }
+  }, [promotion]);
+  useEffect(() => {
     setTotalPrice(calculateTotalPrice(listFood));
-    setTotal(totalPrice + (item.booking.table.type === 'VIP' ? 1000000 : 0) - item.booking.deposit);
   }, [listFood]);
+  useEffect(() => {
+    setTempPrice(totalPrice + (item.booking.table.type === 'VIP' ? 1000000 : 0) - item.booking.deposit);
 
+  }, [totalPrice]);
+  useEffect(() => {
+    handleTotal();
+  }, [tempPrice]);
+  useEffect(() => {
+
+  }, [total]);
   const calculateTotalPrice = (list) => {
     if (list.length === 0) {
       return 0; // Trả về 0 nếu response không hợp lệ
@@ -112,13 +131,39 @@ const BookingDetails = () => {
   const handlePayment = () => {
     payment({
       id: item.booking._id, // id của đơn đặt trước
-      total: totalPrice // số tiền
+      voucher: promotion?.code, // mã giảm giá
+      total // số tiền
     }).then((response) => {
       console.log(response);
       window.open(response.data.DT.payUrl, '_blank');
     })
   }
-  
+  const handleOpenModal = () => {
+    setIsPromotionModalOpen(true);
+  };
+
+  const handleTotal = () => {
+    if (!promotion) {
+      setTotal(tempPrice);
+      return;
+    } else {
+      if (promotion.condition <= tempPrice) {
+        if (promotion.type === 'fixed') {
+          setTotal(tempPrice - promotion.discount);
+        } else {
+          setTotal(tempPrice - (promotion.discount * tempPrice) / 100);
+        }
+      } else {
+        message.error("Đơn hàng không đủ điều kiện để sử dụng mã giảm giá");
+      }
+    }
+  };
+  const handleChange = (e) => {
+    if (!e.target.value) {
+      setPromotion(null);
+      setTotal(tempPrice);
+    }
+  }
   return (
     <div className={styles["page"]}>
       <Flex vertical gap={20}>
@@ -127,8 +172,8 @@ const BookingDetails = () => {
 
         <div className={styles["header-1"]}>Gọi món</div>
         <Flex gap={"1rem"}>
-          <Button onClick={handleAddFood} className={styles["btn"]}>Thêm món</Button>
-          <Button onClick={handlePayment} className={styles["btn"]}>Thanh toán</Button>
+          <Button onClick={handleAddFood} type="primary">Thêm món</Button>
+          <Button onClick={handlePayment} type="default">Thanh toán</Button>
         </Flex>
         <div>
           <Row>
@@ -156,25 +201,31 @@ const BookingDetails = () => {
             </Col>
             <Col span={10} offset={1}>
               <Title level={4}>Thành tiền</Title>
-              <Form layout="inline">
+              <Form form={form} layout="inline">
                 <Form.Item label="Chọn mã giảm giá" name="voucher">
-                  <Input onDoubleClick={handleOpenModal} />
+                  <Input onChange={(e) => handleChange(e)} onDoubleClick={handleOpenModal} />
                 </Form.Item>
                 <Form.Item >
-                  <Button type="primary" htmlType="submit">Áp dụng</Button>
+                  <Button type="primary" onClick={handleTotal}>Áp dụng</Button>
                 </Form.Item>
               </Form>
               <Descriptions column={1} className="mt-5 ps-4">
                 <Descriptions.Item label={<Text style={{ fontWeight: 700 }}>Tiền cọc: </Text>}>{item.booking.deposit.toLocaleString("vi-VN") || 0} VNĐ</Descriptions.Item>
                 <Descriptions.Item label={<Text style={{ fontWeight: 700 }}>Tiền bàn: </Text>}>{item.booking.table.type === 'VIP' ? new Number(1000000).toLocaleString('vi-VN') : 0} VNĐ</Descriptions.Item>
                 <Descriptions.Item label={<Text style={{ fontWeight: 700 }}>Tiền món ăn: </Text>}>{totalPrice.toLocaleString("vi-VN") || 0} VNĐ</Descriptions.Item>
+                {total !== tempPrice && <Descriptions.Item label={<Text style={{ fontWeight: 700 }}>Giảm giá: </Text>}>{promotion?.discount}{promotion?.type === 'fixed' ? "VNĐ" : "%"}</Descriptions.Item>}
                 <Descriptions.Item label={<Text style={{ fontWeight: 700 }}>Thành tiền: </Text>}>
-                  <Text type="danger" delete>{(totalPrice + (item.booking.table.type === 'VIP' ? 1000000 : 0) - item.booking.deposit).toLocaleString("vi-VN") || 0} VNĐ</Text> &nbsp;&nbsp;&nbsp; <Text >{total.toLocaleString("vi-VN") || 0} VNĐ</Text>
+                  <Text hidden={total === tempPrice} type="danger" delete>{(totalPrice + (item.booking.table.type === 'VIP' ? 1000000 : 0) - item.booking.deposit).toLocaleString("vi-VN") || 0} VNĐ</Text> &nbsp;&nbsp;&nbsp; <Text >{total.toLocaleString("vi-VN") || 0} VNĐ</Text>
                 </Descriptions.Item>
               </Descriptions>
             </Col>
 
           </Row>
+          <Promotion
+            isOpen={isPromotionModalOpen}
+            onClose={closePromotionModal}
+            setFieldValue={setPromotion}
+          />
           <OrderModal
             isOpen={isOrderModalOpen}
             onClose={closeOrderModal}
